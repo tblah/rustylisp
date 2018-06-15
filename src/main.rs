18fb2bbcff5528f8317ec3becb6921a40d198737
Tom Eccles <t@freedommail.info>
2018-06-15 14:12:39 +0100

    Handle line comments in token processing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+#![feature(toowned_clone_into)]
 #[cfg_attr(feature = "cargo-clippy", deny(clippy_pedantic))]
 mod tokenise;
 
 fn main() {
-    let test_str = "(+ 1 2 (plus 3 4))";
+    let mut test_str = String::from("; Comment\n(+ 1 2 (plus 3 4))");
     println!("{}", test_str);
 
-    let res: Vec<_> = tokenise::tokenise(test_str).collect();
+    let res: Vec<_> = tokenise::tokenise(&mut test_str).collect();
 
     println!("{:?}", res);
 }
--- src/tokenise.rs
+++ src/tokenise.rs
@@ -2,21 +2,45 @@ use std::iter::FlatMap;
 use std::str::SplitWhitespace;
 
 /// Monster of a type because of the unpleasant way `std::iter` works :-(
-pub type IterableOfStrings<'a> = FlatMap<
-    SplitWhitespace<'a>,
-    Vec<String>,
-    for<'r> fn(&'r str) -> Vec<String>,
->;
+pub type IterableOfStrings<'a> =
+    FlatMap<SplitWhitespace<'a>, Vec<String>, for<'r> fn(&'r str) -> Vec<String>>;
 
 /// Turn scheme source into an iterable of tokens (Strings)
-pub fn tokenise(source: &str) -> IterableOfStrings {
-    source
-        .trim()
-        .split_whitespace() // interior of s-expressions are whitespace separated
+/// source is modified in place removing comments
+pub fn tokenise(source: &mut String) -> IterableOfStrings {
+    let trimmed = trim_comments(source.as_ref());
+    // copy back into the source buffer so that the lifetime of the iterable is sufficient
+    trimmed.clone_into(source);
+    source.split_whitespace() // interior of s-expressions are whitespace separated
         .flat_map(tokenise_brackets) // split at ( ), producing the '(' and ')' tokens as well
 }
 
-// split things like "(cons" into ["(", "cons"]
+/// remove line comments
+fn trim_comments(source: &str) -> String {
+    let mut ret = String::new();
+    let mut in_comment = false;
+
+    for c in source.chars() {
+        if in_comment {
+            if c == '\n' {
+                // comments end at the end of the lines
+                in_comment = false;
+            }
+            continue;
+        }
+
+        // if we are not in a comment
+        if c == ';' {
+            in_comment = true;
+        } else {
+            ret.push(c); // only output non-comment characters
+        }
+    }
+
+    ret
+}
+
+/// split things like "(cons" into ["(", "cons"]
 fn tokenise_brackets(source: &str) -> Vec<String> {
     let mut ret: Vec<String> = Vec::new();
     let mut current = String::new();
@@ -51,7 +75,7 @@ fn tokenise_brackets(source: &str) -> Vec<String> {
 mod tests {
 
     fn run_test(tv: &str, expected: &Vec<&str>) {
-        let res: Vec<String> = super::tokenise(tv).collect();
+        let res: Vec<String> = super::tokenise(&mut String::from(tv)).collect();
         assert_eq!(res, *expected);
     }
 
@@ -69,5 +93,26 @@ mod tests {
             "(apple (b cool) d)",
             &vec!["(", "apple", "(", "b", "cool", ")", "d", ")"],
         );
+    }
+
+    #[test]
+    fn line_comments() {
+        run_test(
+            "(add ; wild comment
+                 1 2 3)",
+            &vec!["(", "add", "1", "2", "3", ")"],
+        )
+    }
+
+    #[test]
+    fn multiple_lines() {
+        run_test(
+            "
+             (one two)
+             ; two three four
+             (five)
+",
+            &vec!["(", "one", "two", ")", "(", "five", ")"],
+        )
     }
 }
